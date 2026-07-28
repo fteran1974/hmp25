3484c12d67691b3082866d61c130b38d0571bbc3
fteran1974 <fteran@math.uc3m.es>
2026-07-28 15:46:02 +0100

    Add files via upload
    Provides the experimental probability that projectionq succeeds in flagging the right number of true finite and infinite eigenvalues, by running it a number of times chose by the user.

---
+++ projectionqprob.m
@@ -0,0 +1,18 @@
+function[prob]=projectionqprob(m,a,b,coeff,k,delta,delta2,delta3,delta4)
+% Runs m times projectionq for a singular quadratic matrix polynomial
+% and provides the empirical probability of getting the right
+% number of true evals
+% a is the number of true finite evals
+% b is the number of true infinite evals
+
+rng(0);
+number=0;
+for i=1:m
+    [~,evals,inf]=projectionq(coeff,k,delta,delta2,delta3,delta4);
+    t1=length(evals);
+    t2=length(inf);
+    if t1==a && t2==b
+        number=number+1;
+    end
+end
+prob=number/m;
